9fd289bd6de170e7c1ccb4cf6d0e2f76caf87722
Shreyansh <shreyanshop20+2@outlook.com>
2026-07-03 22:51:56 +0530

    Time: 0 ms (100%), Space: 19.5 MB (53.08%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,14 +1,14 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        sample = [""]*n
+        sample = []
         for i in range(1, n+1):
-            sample[i-1] = f"{i}"
-
             if i%3 == 0 and i%5 == 0:
-                sample[i-1] = "FizzBuzz"
+                sample.append("FizzBuzz")
             elif i%3 == 0:
-                sample[i-1] = "Fizz"
+                sample.append("Fizz")
             elif i%5 == 0:
-                sample[i-1] = "Buzz"
+                sample.append("Buzz")
+            else:
+                sample.append(str(i))
                 
         return sample
