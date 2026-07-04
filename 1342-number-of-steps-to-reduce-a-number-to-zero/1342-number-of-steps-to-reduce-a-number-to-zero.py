8b77ea6097454097b8dc1f3f6c82f18756887e1d
Shreyansh <shreyanshop20+2@outlook.com>
2026-07-04 15:35:26 +0530

    Time: 2 ms (13.08%), Space: 19.4 MB (23.26%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -4,8 +4,7 @@ class Solution:
         while num != 0:
             if num%2 == 0:
                 num = num/2
-                steps = steps+1
-            elif num%2 != 0:
+            else:
                 num = num-1
-                steps = steps+1
+            steps += 1
         return steps
